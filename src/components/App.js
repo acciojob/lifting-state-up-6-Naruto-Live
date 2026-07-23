63fd0467f03f2b7cf1b95cb4205fa45e765b432c
Supra-Mk4-1998 <sr7773879@gmail.com>
2026-07-23 23:51:40 +0530

    i this is a minor mistake making burder

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,34 @@
-import React , { useState } from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 import TodoList from "./TodoList";
 
 const App = () => {
-  
-  const [todos , setTodos] = useState([
-    {id: 1, text: "Learn React ", completed: false},
-    {id: 2, text: "Complete Assignment ", completed: false},
-    {id: 3, text: "Practice JavaScript ", completed: false}
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false },
   ]);
 
-const handleComplete = (id) => {
-  setTodos(prevTodos =>
-    prevTodos.map(todo =>
-      todo.id === id
-        ? { ...todo, completed: true }
-        : todo
-    )
-  );
-};
+  const handleComplete = (id) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id
+          ? { ...todo, completed: true }
+          : todo
+      )
+    );
+  };
 
   return (
     <div>
-        <TodoList 
+      <h1>Parent Component</h1>
+
+      <TodoList
         todos={todos}
         handleComplete={handleComplete}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,21 +2,23 @@ import React from "react";
 
 const TodoList = ({ todos, handleComplete }) => {
   return (
-    <ul>
-      {todos.map(todo => (
-        <li key={todo.id}>
-          {todo.text}
+    <div>
+      <h2>Child Component</h2>
 
-          {todo.completed ? (
-            <span>Completed</span>
-          ) : (
-            <button onClick={() => handleComplete(todo.id)}>
-              Complete
-            </button>
-          )}
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
